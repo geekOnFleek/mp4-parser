68676582e512b91991326493d55ab0b6c90124d8
Jan Fischer <jan.philip.fischer@geekinbusiness.de>
2019-01-04 21:11:22 +0100

    Now returns the mp4s average color

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ import cv2
 
 cap = cv2.VideoCapture("testfiles/test.mp4")
 i = 0
-while(i < 180):
+length = 180
+avg_color = [0 ,0 ,0]
+while(i < length):
+	acc_color = [0, 0, 0]
 	ret, frame = cap.read()
-	#cv2.imshow('frame', gray)
-	print("Frame " + str(i))	
+	#print("Frame " + str(i))	
 	i += 1
-	cv2.imwrite("testfiles/frameout/frame"+ str(i) + ".jpg", frame)
-	if not cv2.waitKey(10):
-		break
+	#cv2.imwrite("testfiles/frameout/frame"+ str(i) + ".jpg", frame)
+	for x in range(frame.shape[0]):
+		for y in range(frame.shape[1]):
+			acc_color[0] += frame[x][y][0]
+			acc_color[1] += frame[x][y][1]
+			acc_color[2] += frame[x][y][2]
+#	print(acc_color)
+	j = frame.shape[0] * frame.shape[1]
+	avg_color[0] += acc_color[0] / (length * j)
+	avg_color[1] += acc_color[1] / (length * j)
+	avg_color[2] += acc_color[2] / (length * j)
+#	print(avg_color)
 
+print(avg_color)
 cap.release()
 cv2.destroyAllWindows()
