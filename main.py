f296f46a7d72d73914d51ec5166bea541a5a7d7a
Jan Fischer <jan.philip.fischer@geekinbusiness.de>
2019-01-04 18:39:34 +0100

    main.py does now read

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+import cv2
 
+cap = cv2.VideoCapture("testfiles/test.mp4")
+i = 0
+while(i < 180):
+	ret, frame = cap.read()
+	#cv2.imshow('frame', gray)
+	print("Frame " + str(i))	
+	i += 1
+	cv2.imwrite("testfiles/frameout/frame"+ str(i) + ".jpg", frame)
+	if not cv2.waitKey(10):
+		break
 
+cap.release()
+cv2.destroyAllWindows()
